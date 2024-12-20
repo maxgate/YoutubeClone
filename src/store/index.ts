6a602e832bdacad8117122ae398c34516b4edd53
maxgate <kelechivincy@gmail.com>
2024-12-20 15:46:28 +0100

    updated the gitignore

--- src/store/index.ts
+++ src/store/index.ts
@@ -23,5 +23,5 @@ export const store = configureStore({
   },
 });
 
-export type RootS tate = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
